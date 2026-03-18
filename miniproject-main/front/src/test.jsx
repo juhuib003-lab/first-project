3e7bcedacb06553bc11b90001c9944ff08572fbd
juhuib003-lab <juhuib003@gmail.com>
2026-03-18 16:39:37 +0900

    제발!!!

--- miniproject-main/front/src/test.jsx
+++ miniproject-main/front/src/test.jsx
@@ -1,10 +1,5 @@
 const Test = () => {
-  return (
-    <div>
-      <h1>Test Component</h1>
-      <p>This is a simple test component.</p>
-    </div>
-  );
-};
+}       
+return 0;
 
 export default Test;
